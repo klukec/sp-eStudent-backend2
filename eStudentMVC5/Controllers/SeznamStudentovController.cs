bbddb7276e10fe235a6bb675f279083d2505a3cd
klukec <maticnova@gmail.com>
2015-12-15 23:48:42 +0100

    Grid View za silo dela.

--- eStudentMVC5/Controllers/SeznamStudentovController.cs
+++ eStudentMVC5/Controllers/SeznamStudentovController.cs
@@ -8,10 +8,23 @@ namespace eStudentMVC5.Controllers
 {
     public class SeznamStudentovController : Controller
     {
+        estudentEntities db = new estudentEntities();
+
         // GET: SeznamStudentov
         public ActionResult Index()
         {
-            return View();
+            var students = from s in db.uporabnik where s.idVloge.Equals(1) select s;
+            return View(students.ToList());
+            
+            //var students = from s in db.uporabnik where s.idVloge.Equals(1) select s;
+            //ViewData["students"] = students;
+            //return View();
+
+            //IEnumerable<uporabnik> students2 = students;
+            //ViewData["students2"] = students2;
+            //return View();
+
         }
+
     }
 }
