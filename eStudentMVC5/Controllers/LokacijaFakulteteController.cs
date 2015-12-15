7bb15d9da9a0f7f7340b25aa603edcbd18ac821f
klukec <maticnova@gmail.com>
2015-12-15 15:25:42 +0100

    Statične strani delajo.

---
+++ eStudentMVC5/Controllers/LokacijaFakulteteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eStudentMVC5.Controllers
+{
+    public class LokacijaFakulteteController : Controller
+    {
+        // GET: LokacijaFakultete
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
