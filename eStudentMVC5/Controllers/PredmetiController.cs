9f230dcf1170189a96147e2652b7e813f0b27b1a
klukec <maticnova@gmail.com>
2015-12-21 23:48:13 +0100

    Matram se s predmeti.

---
+++ eStudentMVC5/Controllers/PredmetiController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eStudentMVC5.Controllers
+{
+    public class PredmetiController : Controller
+    {
+        estudentEntities db = new estudentEntities();
+
+        // GET: Predmeti
+        public ActionResult Index()
+        {
+            // Spodnja forma za dodajanje oz. urejanje predmeta.
+            var prof = from s in db.uporabnik where s.idVloge.Equals(2) select s;
+            List<uporabnik> profesorji = prof.ToList();
+            IEnumerable<SelectListItem> selectProfesor = from c in profesorji select new SelectListItem
+                {
+                    Selected = (c.idUporabnik == 0),
+                    Text = c.ime + " " + c.priimek,
+                    Value = c.idUporabnik.ToString()
+                };
+            ViewData["profesorji"] = selectProfesor;
+
+            // Zgornja tabela predmetov.
+            var predmeti = from s in db.predmet select s;
+            List<predmet> predmetiR = predmeti.ToList();
+
+            // Vrni Tuple na View.
+            var tuple = new Tuple<List<predmet>, predmet>(predmetiR, new predmet());
+            return View(tuple);
+
+        }
+
+        [HttpPost]
+        public ActionResult Posodobi(FormCollection collection)
+        {
+            string var1 = collection["imePredmeta"];
+            return View(); 
+        }
+
+    }
+}
---
+++ eStudentMVC5/Controllers/SeznamZaposlenihController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eStudentMVC5.Controllers
+{
+    public class SeznamZaposlenihController : Controller
+    {
+        estudentEntities db = new estudentEntities();
+
+        // GET: SeznamZaposlenih
+        public ActionResult Index()
+        {
+            var employees = from s in db.uporabnik where !s.idVloge.Equals(1) select s;
+            return View(employees.ToList());
+        }
+    }
+}
