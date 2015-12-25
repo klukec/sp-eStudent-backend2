02e3c420f59a20a0bcdd9bbdcc6d978482f26d77
klukec <maticnova@gmail.com>
2015-12-25 17:25:56 +0100

    Del "Razpisani izpitni roki" že na pol dela.

--- eStudentMVC5/Controllers/IndeksController.cs
+++ eStudentMVC5/Controllers/IndeksController.cs
@@ -9,13 +9,40 @@ namespace eStudentMVC5.Controllers
     [Authorize]
     public class IndeksController : Controller
     {
+        estudentEntities db = new estudentEntities();
+
         // GET: Indeks
         public ActionResult Index()
         {
-            var dbContext = new estudentEntities();
-            var user = dbContext.uporabnik.Find(1);
-            string ime = user.ime;
-            ViewBag.imeUporabnika = ime;
+            try
+            {
+                uporabnik tren = (from s in db.uporabnik where s.email.Equals(User.Identity.Name) select s).ToList().First();
+                List<ocena> opravljeniPredmeti = (from s in db.ocena where (s.ocena1 > 5 && s.idStudenta == tren.idUporabnik) select s).ToList();
+                ViewBag.opravljeniPredmeti = opravljeniPredmeti;
+
+                double stOpravljenih = opravljeniPredmeti.Count();
+                ViewBag.stOpravljenih = stOpravljenih;
+
+                int sumKreditne = 0;
+                foreach (ocena i in opravljeniPredmeti)
+                {
+                    sumKreditne += i.predmet.stKreditnih;
+                }
+                ViewBag.sumKreditne = sumKreditne;
+
+                double povprecje = 0;
+                foreach (ocena i in opravljeniPredmeti)
+                {
+                    povprecje += i.ocena1;
+                }
+                povprecje = povprecje / stOpravljenih;
+                ViewBag.povprecje = povprecje;
+            }
+            catch
+            {
+                Log.Error("Napaka pri izdelavi elektronskega indeksa.");
+            }
+
             return View();
         }
     }
---
+++ eStudentMVC5/Controllers/RazpisaniRokiController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eStudentMVC5.Controllers
+{
+    [Authorize]
+    public class RazpisaniRokiController : Controller
+    {
+        estudentEntities db = new estudentEntities();
+
+        // GET: RazpisaniRoki
+        public ActionResult Index()
+        {
+            try
+            {
+                List<izpitnirok> izpitniRoki = (from s in db.izpitnirok select s).ToList();
+                ViewBag.izpitniRoki = izpitniRoki;
+            }
+            catch
+            {
+                Log.Error("Napaka pri pripravi izpitnih rokov.");
+            }
+
+            return View();
+        }
+    }
+}
